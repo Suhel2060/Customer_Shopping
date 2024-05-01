2fbd3ac6b4127c600130e68079096eedfa521a05
suhel2060 <maharjansohail222@gmail.com>
2024-05-01 08:18:09 +0545

    generate package changed

--- appbackend/routes/generatepackage.js
+++ appbackend/routes/generatepackage.js
@@ -2,67 +2,65 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/generatepackage', (req, res) => {
-  const items = req.body;
-  let currentPackage = { items: [], totalWeight: 0, totalCost: 0, courierCost: 0 };
-  const packages = generatepackage(items)
-  res.json(packages);
+  //get the data from th body 
+  try {
+    const items = req.body;
+    //calling the generatePackage Function
+    const packages = generatepackage(items)
+    res.json(packages);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 
 
 
   function generatepackage(items) {
-    let totalCost = 0;
     let packages = [];
-
-    items.forEach((element) => {
-      totalCost += element.cost;
+    let currentPackage = { items: [], totalWeight: 0, totalCost: 0, courierCost: 0 };
+    items.sort((a, b) => (b.cost / b.weight) - (a.cost / a.weight));
+    items.forEach((item) => {
+      // checking whether the totalcost exceed 250 or not
+      if (currentPackage.totalCost + item.cost > 250) {
+        currentPackage.courierCost = courierPrice(currentPackage.totalWeight)
+        packages.push(currentPackage);
+        currentPackage = { items: [], totalWeight: 0, totalCost: 0, courierCost: 0 };
+        insertItems(currentPackage, item)
+      } else if (currentPackage.totalCost + item.cost < 250) {
+        insertItems(currentPackage, item);
+      }
     });
 
-    if (totalCost > 250) {
-      items.sort((a, b) => (b.cost / b.weight) - (a.cost / a.weight));
-    }
-
-    items.forEach((item) => {
-      if (currentPackage.totalCost + item.cost > 250) {
+    // Push the last package into the packages array
+    if (currentPackage.items.length > 0) {
       currentPackage.courierCost = courierPrice(currentPackage.totalWeight)
       packages.push(currentPackage);
-      currentPackage = { items: [], totalWeight: 0, totalCost: 0, courierCost: 0 };
-      insertItems(item)
-    } else if (currentPackage.totalCost + item.cost < 250) {
-      insertItems(item);
     }
-  });
-
-// Push the last package into the packages array
-if (currentPackage.items.length > 0) {
-  currentPackage.courierCost = courierPrice(currentPackage.totalWeight)
-  packages.push(currentPackage);
-}
-return packages;
+    return packages;
   }
 
 
+//adding data to the current package
+  function insertItems(currentPackage, item) {
+    currentPackage.items.push(item.item);
+    currentPackage.totalWeight += item.weight;
+    currentPackage.totalCost += item.cost;
+  }
 
-function insertItems(item) {
-  currentPackage.items.push(item.item);
-  currentPackage.totalWeight += item.weight;
-  currentPackage.totalCost += item.cost;
-}
-
-
-function courierPrice(weight) {
-  if (weight > 0 && weight < 200) {
-    return 5
+  // getting the courier orice for packages
+  function courierPrice(weight) {
+    if (weight > 0 && weight < 200) {
+      return 5
+    }
+    else if (weight >= 200 && weight < 500) {
+      return 10
+    }
+    else if (weight >= 500 && weight < 1000) {
+      return 15
+    }
+    else if (weight > 1000 && weight <= 5000) {
+      return 20
+    }
   }
-  else if (weight >= 200 && weight < 500) {
-    return 10
-  }
-  else if (weight >= 500 && weight < 1000) {
-    return 15
-  }
-  else if (weight > 1000 && weight <= 5000) {
-    return 20
-  }
-}
 });
 
 
